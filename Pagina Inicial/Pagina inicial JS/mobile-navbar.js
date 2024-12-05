8f7d1b4d58b912903192aca8cc973e255e4786f8
AMSDAF <araujomtloko@gmail.com>
2024-12-05 00:35:00 -0300

    SiteYokami-V0.02

--- Pagina Inicial/Pagina inicial JS/mobile-navbar.js
+++ Pagina Inicial/Pagina inicial JS/mobile-navbar.js
@@ -1,29 +1,40 @@
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
+    constructor(mobileMenu, navList, navLinks, menuLines) {
         this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList)
-        this.navLinks = document.querySelectorAll(navLinks)
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.menuLines = document.querySelectorAll(menuLines);
         this.activeClass = "active";
-
         this.handleClick = this.handleClick.bind(this);
     }
 
-animateLinks() {
-    this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
-    })
-}
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
+
+    toggleMenuLinesColor() {
+        this.menuLines.forEach((line) => {
+            if (this.mobileMenu.classList.contains(this.activeClass)) {
+                line.style.backgroundColor = "black";
+            } else {
+                line.style.backgroundColor = "";
+            }
+        });
+    }
 
     handleClick() {
         this.navList.classList.toggle(this.activeClass);
         this.mobileMenu.classList.toggle(this.activeClass);
         this.animateLinks();
+        this.toggleMenuLinesColor();
     }
 
     addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick)
+        this.mobileMenu.addEventListener("click", this.handleClick);
     }
 
     init() {
@@ -38,6 +49,7 @@ const mobileNavbarInstance = new MobileNavbar(
     ".mobile-menu",
     ".nav-list",
     ".nav-list li",
+    ".mobile-menu div"
 );
 
 mobileNavbarInstance.init();
