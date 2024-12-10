1dbc874c2497bee38b39f642f5f9497747ee7bbb
AMSDAF <araujomtloko@gmail.com>
2024-12-10 12:18:02 -0300

    SiteYokami-V0.20

--- src/Sistemas/Clans/JavaScript/clan-button.js
+++ src/Sistemas/Clans/JavaScript/clan-button.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const mainButton = document.querySelector('.main-button');
-    const contentButton = document.querySelector('.clanlist-content');
-    const setaButton = document.querySelector('.clanlist-seta');
+    const contentButton = document.querySelector('.button-content');
+    const setaButton = document.querySelector('.seta-button');
 
     if (mainButton && contentButton) {
         document.addEventListener('click', function() {
